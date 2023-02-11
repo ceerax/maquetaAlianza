ede5203463b5bc044d3ebb348c58bf26e53515a7
ceerax <cesarmoreno.20@hotmial.com>
2023-02-11 18:26:32 -0500

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "../img/img1.png";
+export const Login = () => {
+  return (
+    <div>
+      <section className="img"></section>
+      <section className="form-Right">
+        <form action="">
+          <h2>ejemplo</h2>
+          <label htmlFor="usuario">Email</label>
+          <input type="text" name="usuario" />
+        </form>
+      </section>
+    </div>
+  );
+};
