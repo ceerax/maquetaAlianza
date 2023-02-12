e46ee4103a8e70d80ab36884cb065bf2e8babf74
ceerax <cesarmoreno.20@hotmial.com>
2023-02-11 20:18:50 -0500

    crete login and adjust styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import Image from "../img/img1.png";
 export const Login = () => {
   return (
-    <div>
-      <section className="img"></section>
+    <>
+      <section className="img-left">
+        <h1>
+          Bienvenido a la mejor plataforma <br />
+          <span>organizacional online</span>
+        </h1>
+
+        <h3>Gestíon efectiva del talento</h3>
+      </section>
       <section className="form-Right">
         <form action="">
           <h2>ejemplo</h2>
           <label htmlFor="usuario">Email</label>
           <input type="text" name="usuario" />
+          <label htmlFor="usuario">Passwors</label>
+          <input type="Password" name="Passwors" />
+          <div className="check">
+            <input type="checkbox" name="checkbox" />
+            <span>Recordar</span>
+          </div>
+          <input type="submit" value="Ingresar" />
         </form>
       </section>
-    </div>
+    </>
   );
 };
