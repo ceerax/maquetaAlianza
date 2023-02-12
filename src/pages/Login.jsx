516af754fd24352a993e01c523a61a832bb490e0
ceerax <cesarmoreno.20@hotmial.com>
2023-02-12 00:43:08 -0500

    style correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,51 @@
 import React from "react";
-import Image from "../img/img1.png";
+import Logo from "../img/Vector.png";
+import Tex from "../img/textLogo.png";
+import Modal from "../components/Modal";
 export const Login = () => {
   return (
     <>
       <section className="img-left">
         <h1>
           Bienvenido a la mejor plataforma <br />
-          <span>organizacional online</span>
+          <span className="line">organizacional online</span>
         </h1>
 
         <h3>Gestíon efectiva del talento</h3>
       </section>
       <section className="form-Right">
         <form action="">
-          <h2>ejemplo</h2>
-          <label htmlFor="usuario">Email</label>
-          <input type="text" name="usuario" />
-          <label htmlFor="usuario">Passwors</label>
-          <input type="Password" name="Passwors" />
-          <div className="check">
-            <input type="checkbox" name="checkbox" />
+          <div className="content-Logo">
+            <div>
+              <img className="logo" src={Logo} alt="" />
+              <img className="tex-Logo" src={Tex} alt="" />
+            </div>
+          </div>
+          <label htmlFor="usuario">Usuario</label>
+          <input
+            type="text"
+            name="usuario"
+            placeholder="ingresa tu nombre de usuario"
+          />
+          <label htmlFor="usuario">Contraseña</label>
+          <input
+            type="Password"
+            name="Passwors"
+            placeholder="Escribe tu contraseña"
+          />
+          <div className="check d-flex justify-content-center">
+            <input className="mx-2" type="checkbox" name="checkbox" />
             <span>Recordar</span>
           </div>
           <input type="submit" value="Ingresar" />
+          <div class="row mt-3 justify-content-between container-rescue">
+            <div class="col-4 p-0 ">
+              <a href="/">¿Olvidaste tu usuario?</a>{" "}
+            </div>
+            <div class="col-5 p-0 d-flex justify-content-end">
+              <a href="/">¿Olvidaste tu contraseña?</a>{" "}
+            </div>
+          </div>
         </form>
       </section>
     </>
